70828eef169b9f79dac89a5061da3da46ddea4e1
Willian <dasilva.williansoares@gmail.com>
2020-11-23 17:02:45 -0300

    Post Item

--- ToDoList.Api/ToDoList.Api/Startup.cs
+++ ToDoList.Api/ToDoList.Api/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ToDoList.Api.Models;
 
 namespace ToDoList.Api
 {
@@ -25,6 +27,8 @@ namespace ToDoList.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+               opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
         }
 
